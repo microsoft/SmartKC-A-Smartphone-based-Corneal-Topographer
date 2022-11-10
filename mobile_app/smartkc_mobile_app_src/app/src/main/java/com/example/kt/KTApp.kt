affb5009eca5b796fcf00ae09ce88754d8b78080
Anurag Shukla <shukla.anurag0006@gmail.com>
2022-11-11 00:00:34 +0530

    Add custom worker

--- mobile_app/smartkc_mobile_app_src/app/src/main/java/com/example/kt/KTApp.kt
+++ mobile_app/smartkc_mobile_app_src/app/src/main/java/com/example/kt/KTApp.kt
@@ -1,7 +1,17 @@
 package com.example.kt
 
 import android.app.Application
+import androidx.work.Configuration
+import com.example.kt.data.manager.UploadDelegatingWorkerFactory
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class KTApp: Application() {}
+class KTApp: Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: UploadDelegatingWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder().setMinimumLoggingLevel(android.util.Log.DEBUG).setWorkerFactory(workerFactory).build()
+}
